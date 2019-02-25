668ed665aef2cd9bf5816a68ea1252a60574340b
Kamnag R <rkamnag@gmail.com>
2019-02-25 17:42:37 +0530

    Add files via upload

---
+++ anagram_dhoni.py
@@ -0,0 +1,9 @@
+s = input()
+s = s.lower()
+if len(s)==5:
+    if 'd' in s and 'o' in s and 'h' in s and 'n' in s and 'i' in s:
+        print("yes")
+    else:
+        print("no")
+else:
+    print("no")
